1c3578aa204a9400cbc369fb800209030ff6d6d1
jimin <jjimini98@naver.com>
2021-03-18 23:30:18 +0900

    finished rockpapaerscissors

--- CplusplusVS/rockpapaerscissors.cpp
+++ CplusplusVS/rockpapaerscissors.cpp
@@ -1,48 +1,45 @@
+
+//#include <iostream>
+//#include <stdlib.h>
+//#include <ctime>
+//using namespace std;
 //
-// Created by jimin on 3/18/2021.
+//int alice;
+//int bob;
 //
-
-#include <iostream>
-#include <stdlib.h>
-#include <ctime>
-using namespace std;
-
-int alice;
-int bob;
-
-int main() {
-
-
-    srand((unsigned int)time(NULL));
-    alice = rand() % 3;
-    //cout << alice << endl; //나중에 주석처리
-
-    cout << "Bob의 값을 입력하시오(0:가위, 1:바위 , 2:보) >>";
-    cin >> bob;
-    while (1) {
-        if (bob == 0) { //가위
-            if (alice == 1) { cout << "alice가 바위를 내었고 bob이 가위를 내어서 alice가 이겼습니다" << endl; }
-            else if (alice == 2) { cout << "alice가 보를 내었고 bob이 가위를 내어서 bob이 이겼습니다" << endl; break; }
-            else { cout << "alice가 가위를 냈고 bob이 가위를 내서 서로 비겼습니다" << endl; }
-        }
-        else if (bob == 1) { // 바위
-            if (alice == 1) { cout << "alice가 바위를 내었고 bob이 바위를 내어서 서로 비겼습니다" << endl; }
-            else if (alice == 2) { cout << "alice가 보를 내었고 bob이 바위 내어서 alice가 이겼습니다" << endl; }
-            else { cout << "alice가 가위를 냈고 bob이 바위를 내서 bob이 이겼습니다" << endl; break; }
-        }
-        else {//보
-            if (alice == 1) { cout << "alice가 바위를 내었고 bob이 보를 내어서 bob이 이겼습니다" << endl; break; }
-            else if (alice == 2) { cout << "alice가 보를 내었고 bob이 보를내어서 서로 비겼습니다" << endl; }
-            else { cout << "alice가 가위를 냈고 bob이 보를 내서 alice가 이겼습니다" << endl; }
-
-        }
-
-    }
-
-
-
-}
-
-
-
-
+//int main() {
+//
+//
+//    srand((unsigned int)time(NULL));
+//    alice = rand() % 3;
+//    //cout << alice << endl; //나중에 주석처리
+//
+//    cout << "Bob의 값을 입력하시오(0:가위, 1:바위 , 2:보) >>";
+//    cin >> bob;
+//    while (1) {
+//        if (bob == 0) { //가위
+//            if (alice == 1) { cout << "alice가 바위를 내었고 bob이 가위를 내어서 alice가 이겼습니다" << endl; }
+//            else if (alice == 2) { cout << "alice가 보를 내었고 bob이 가위를 내어서 bob이 이겼습니다" << endl; break; }
+//            else { cout << "alice가 가위를 냈고 bob이 가위를 내서 서로 비겼습니다" << endl; }
+//        }
+//        else if (bob == 1) { // 바위
+//            if (alice == 1) { cout << "alice가 바위를 내었고 bob이 바위를 내어서 서로 비겼습니다" << endl; }
+//            else if (alice == 2) { cout << "alice가 보를 내었고 bob이 바위 내어서 alice가 이겼습니다" << endl; }
+//            else { cout << "alice가 가위를 냈고 bob이 바위를 내서 bob이 이겼습니다" << endl; break; }
+//        }
+//        else {//보
+//            if (alice == 1) { cout << "alice가 바위를 내었고 bob이 보를 내어서 bob이 이겼습니다" << endl; break; }
+//            else if (alice == 2) { cout << "alice가 보를 내었고 bob이 보를내어서 서로 비겼습니다" << endl; }
+//            else { cout << "alice가 가위를 냈고 bob이 보를 내서 alice가 이겼습니다" << endl; }
+//
+//        }
+//
+//    }
+//
+//
+//
+//}
+//
+//
+//
+//
