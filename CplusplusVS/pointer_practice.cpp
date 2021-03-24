1a6baca92e4b8e17c1690227169281a5dee15c23
jimin <jjimini98@naver.com>
2021-03-24 14:30:07 +0900

    commit

--- CplusplusVS/pointer_practice.cpp
+++ CplusplusVS/pointer_practice.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-	char data[4] = "abc"; 
-	char* p1 = data;
-	char* p2;
-
-	p2 = &data[0];
-
-	cout << *p1 << "," << *p2 << endl;
-	system("pause");
-
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//	char data[4] = "abc"; 
+//	char* p1 = data;
+//	char* p2;
+//
+//	p2 = &data[0];
+//
+//	cout << *p1 << "," << *p2 << endl;
+//	system("pause");
+//
+//
+//}
---
+++ enc_temp_folder/1d2fb07fbfeb25b13bc957a2daba3eb1/prac.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	char data[4] = "abc";
+	char* p = data; 
+
+	char point1 = *p;
+	char point2 = *p++;
+	char point3 = *++p;
+
+	cout << point1 << ", " << point2 << ", " << point3 << endl;
+
+	system("pause");
+
+
+
+}
