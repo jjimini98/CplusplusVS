4def8abd1979ae294df51b2f0cda3dba6ebefcf5
jimin <jjimini98@naver.com>
2021-03-25 18:04:20 +0900

    commit

--- CplusplusVS/prac.cpp
+++ CplusplusVS/prac.cpp
@@ -21,7 +21,20 @@
 
 
 
-#include <iostream>
-using namespace std;
-
-int main(vo)
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int* p, sum = 0;
+//	int num[10] = { 1,2,3,4,5,6,7,8,9,10 }; 
+//	for (p = num; p < &num[10]; p++)
+//		cout << p << endl;
+//	cout << "sum = " << sum << endl;
+//
+//	system("pause");
+//
+//
+//
+//
+//}
