f1a008a2b08d094aae2ebb14c689f90757581dcd
jimin <jjimini98@naver.com>
2021-04-02 10:14:45 +0900

    modified initalfunction.cpp

---
+++ CplusplusVS/Week5/initialfunction.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>;
+//using namespace std;
+//
+//class Circle {
+//
+//public:
+//	int radius; 
+//	Circle();
+//	Circle(int r);
+//	double getArea();
+//
+//};
+//
+//Circle::Circle() : Circle(1) {}
+//
+//Circle::Circle(int r) {
+//
+//	radius = r;
+//	cout << "¹ÝÁö¸§ "; 
+//}
