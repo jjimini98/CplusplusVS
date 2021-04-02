f51559abc6aea62fa35f2ba41e800c3309382745
jimin <jjimini98@naver.com>
2021-04-02 11:24:30 +0900

    finished inital_practice.cpp

---
+++ CplusplusVS/Week5/inital_practice.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+//class rectangle {
+//	int width, height;
+//
+//public:
+//	rectangle() ;
+//	rectangle(int height, int width) { height = height; width = width; }
+//	rectangle(int x) : height(x), width(x) {}
+//};
+//
+//int main() {
+//	rectangle rect1;
+//	rectangle rect2(4, 5);
+//	rectangle rect3(5);
+//
+//	cout << rect1 << endl;
+//	cout << rect2 << endl;
+//	cout << rect3 << endl;
+//
+//
+//
+//}
+
+//class Circle {
+//
+//public:
+//	int radius; 
+//
+//	Circle();
+//	Circle(int r);
+//	~Circle(); // 소멸자
+//	double getArea();
+//};
+//
+//Circle::Circle() {
+//	radius = 1;
+//	cout << "반지름" << radius << "원 생성" << endl;
+//}
+//
+//Circle::Circle(int r) {
+//	radius = r;
+//	cout << "반지름" << radius << "원 생성" << endl; }
+//
+//Circle::~Circle() {
+//	cout << "반지름 " << radius << "원 소멸" << endl;}
+//
+//double Circle::getArea() {
+//	return 3.14 * radius * radius;
+//}
+//
+//int main() { Circle dount; Circle pizza(30); return 0;  }
+//
