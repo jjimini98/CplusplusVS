fbef79a7b14e2e1eac607edfd1edb9bf1cccb7f8
jimin <jjimini98@naver.com>
2021-04-07 15:09:10 +0900

    modifed 접근지정자 .cpp

---
+++ CplusplusVS/Week5/접근지정자.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//using namespace std;
+//
+//class Circle {
+//public:
+//	int radius; 
+//	Circle();
+//	Circle(int r);
+//	double getArea();
+//};
+//
+//Circle::Circle() { radius = 1; }
+//Circle::Circle(int r) {radius = r; }
+//
+//int main() { Circle waffle; waffle.radius = 5; cout << waffle.radius << "ÀÔ´Ï´Ù";  }
