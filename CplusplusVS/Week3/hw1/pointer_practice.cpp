4017cb1dedbe5bc672bb6114b73a2eb494872442
jimin <jjimini98@naver.com>
2021-04-10 02:53:09 +0900

    moved files

---
+++ CplusplusVS/Week3/hw1/pointer_practice.cpp
@@ -0,0 +1,49 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//	char data[4] = "abc"; 
+//	char* p1 = data;
+//	char* p2;
+//
+//	p2 = &data[0];
+//
+//	cout << *p1 << "," << *p2 << endl;
+//	system("pause");
+//
+//
+//}
+
+
+
+// & 연산자와 연산자 - 포인터 
+
+// & 연산자 : 변수 앞에 붙어서 변수가 저장된 주소를 전달한다.
+//#include <iostream>
+//using namespace std;
+//int main(void) {
+//
+//	int* a, b;
+//	a = &b;
+//	cout << "a의 결과값은" << a << endl;
+//
+// 
+// 
+// 
+//}
+
+// * 연산자 : 변수 앞에 붙어서 변수에 저장된 값을 전달한다.
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int *a, b , c;
+//	a = &b; 
+//	b = 30;
+//	c = *a;
+//	cout << "a의 값은 " << a << "b의 값은" << b << "c의 값은" << c << endl; 
+//
+//
+//
+//}
---
+++ CplusplusVS/Week3/hw1/prac.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	char data[4] = "abc";
+//	char* p = data; 
+//
+//	char point1 = *p;
+//	char point2 = *p++;
+//	char point3 = *++p;
+//
+//	cout << point1 << ", " << point2 << ", " << point3 << endl;
+//
+//	system("pause");
+//
+
+
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//
+//	int* p, sum = 0;
+//	int num[10] = { 1,2,3,4,5,6,7,8,9,10 }; 
+//	for (p = num; p < &num[10]; p++)
+//		cout << p << endl;
+//	cout << "sum = " << sum << endl;
+//
+//	system("pause");
+//
+//
+//
+//
+//}
